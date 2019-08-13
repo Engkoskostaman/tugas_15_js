e2fb8a37a57b75c58770c7ce3a1d610f394f3882
Engkos <engkoskostaman97@gmail.com>
2019-08-13 08:24:25 +0700

    tugas 15 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas15.js/index.js
@@ -0,0 +1,4 @@
+var x=2;
+console.log("Niali AB:",Math.pow(x,3));
+console.log("Nilai BC:",Math.sqrt(36));
+console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah :"+Math.sqrt(100)+" cm ");
